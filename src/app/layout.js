c15750433996874f0ab184c094b01d316d217d89
Idaksh <idaksh.6@idaksh.in>
2026-04-07 15:44:06 +0530

    Meta Pixel Code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -61,6 +61,25 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-F3Y6QWCT7N');
           `}
         </Script>
+        {/* Meta Pixel Code */}
+        <Script>
+          {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1721803465861440');
+          fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1721803465861440&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Meta Pixel Code */}
       </head>
       <body className={`${inter.variable} ${outfit.variable} ${spaceGrotesk.variable} antialiased text-white`}>
         <Header />
