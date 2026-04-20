e830c41caecad0ea2c6c33b01cc89e9263f76925
Team IDaksh <idaksh.6@idaksh.in>
2026-04-20 17:38:36 +0530

    Fix mobile Hero banner containment and image positioning

--- src/app/service/[slug]/WordPressDevelopmentPremiumStaging.jsx
+++ src/app/service/[slug]/WordPressDevelopmentPremiumStaging.jsx
@@ -293,13 +293,13 @@ export default function WordPressDevelopmentPremiumStaging({ service, servicesDa
                 <div className="absolute -top-20 -right-20 w-64 h-64 bg-blue-400 rounded-full decorative-blob" />
                 <div className="absolute -bottom-20 -left-20 w-80 h-80 bg-indigo-300 rounded-full decorative-blob" style={{ animationDelay: '-5s' }} />
 
-                <div className="relative h-[350px] sm:h-[450px] lg:h-[600px] w-full">
-                  <div className="absolute top-0 right-0 w-[95%] lg:w-[500px] h-[85%] lg:h-[500px] image-card image-3d-card z-10 border-4 sm:border-8 border-white shadow-2xl">
+                <div className="relative h-[300px] sm:h-[450px] lg:h-[600px] w-full px-4 sm:px-0">
+                  <div className="absolute top-0 right-4 lg:right-0 w-[80%] lg:w-[500px] h-[80%] lg:h-[500px] image-card image-3d-card z-10 border-4 sm:border-8 border-white shadow-2xl">
                     <img src="/wp_hero_aurora.png" alt="WordPress Engineering" className="w-full h-full object-cover" />
                     <div className="absolute inset-0 bg-gradient-to-tr from-blue-900/10 to-transparent" />
                   </div>
 
-                  <div className="absolute -bottom-6 -left-6 w-[65%] lg:w-[350px] h-[55%] lg:h-[280px] image-card wp-float2 z-20 shadow-[0_30px_60px_-15px_rgba(0,0,0,0.3)] border-2 sm:border-4 border-white transform -rotate-3 hover:rotate-0 transition-transform duration-500">
+                  <div className="absolute bottom-2 left-4 lg:-bottom-6 lg:-left-6 w-[55%] lg:w-[350px] h-[50%] lg:h-[280px] image-card wp-float2 z-20 shadow-[0_30px_60px_-15px_rgba(0,0,0,0.3)] border-2 sm:border-4 border-white transform -rotate-3 hover:rotate-0 transition-transform duration-500">
                     <img src="/wp_hero_floating_new.png" alt="Code Quality" className="w-full h-full object-cover" />
                   </div>
 
